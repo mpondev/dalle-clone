f79dd8cc5bd6dc8cd8ce6ae8d2f327323b7b0dbf
Mario Ponce <mario@mpondev.com>
2023-10-29 19:09:11 +0100

    Add react-router-dom and Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import CreatePost from './pages/CreatePost/CreatePost';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'create-post',
+    element: <CreatePost />,
+  },
+]);
 
 function App() {
-  return <div>DALL·E clone</div>;
+  return (
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
