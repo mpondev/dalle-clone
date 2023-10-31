22ffa0769e5483c6e5b3f5f2f27536857ba123f5
Mario Ponce <mario@mpondev.com>
2023-10-31 20:21:20 +0100

    Finish CreatePost routes and get method

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -2,13 +2,13 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 router.route('/').get((req, res) => {
   res.send('Hello from DALL·E post');
@@ -18,17 +18,19 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const image = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    // const image = aiResponse.data.data[0].b64_json;
+
+    console.log(req, res, image);
 
     res.status(200).json({ photo: image });
-  } catch (err) {
+  } catch (error) {
     console.log(error);
     res.status(500).send(error?.response.data.error.message);
   }
