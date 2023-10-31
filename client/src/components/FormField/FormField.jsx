55d4fbaa015cd9574256545454dfbd854c0ac719
mpondev <mario@mpondev.com>
2023-10-31 12:10:40 +0100

    Build FormField component

--- client/src/components/FormField/FormField.jsx
+++ client/src/components/FormField/FormField.jsx
@@ -1,7 +1,41 @@
 import styles from './FormField.module.scss';
 
-function FormField() {
-  return <div className={styles.formfield}>FormField</div>;
+function FormField({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) {
+  return (
+    <div className={styles.formfield}>
+      <div className={styles.fields}>
+        <label htmlFor={name}>{labelName}</label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className={styles.surprise}
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        className={styles.input}
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+      />
+    </div>
+  );
 }
 
 export default FormField;
