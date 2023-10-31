a7e4db16de207e47fab15cb7e8f85f9b62dfd462
Mario Ponce <mario@mpondev.com>
2023-10-31 18:20:39 +0100

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
 
 const app = express();
@@ -12,10 +14,21 @@ app.get('/', async (req, res) => {
   res.send('Hello from DALL·E');
 });
 
+const DB = process.env.MONGODB_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+const PORT = process.env.PORT || 8080;
+
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log('Server has started on port http://localhost:8080')
-  );
+  try {
+    connectDB(DB);
+    app.listen(PORT, () =>
+      console.log('Server has started on http://localhost:8080')
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 startServer();
